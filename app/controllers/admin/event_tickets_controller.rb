7aebfb78e30cf34847f9bbbb5b33e7766a384fc6
keyilianxi <keyilianxi@sina.cn>
2017-10-22 17:28:56 +0800

    test

--- app/controllers/admin/event_tickets_controller.rb
+++ app/controllers/admin/event_tickets_controller.rb
@@ -5,5 +5,46 @@ class Admin::EventTicketsController < AdminController
     @tickets = @event.tickets
   end
 
-  
+  def new
+    @ticket = @event.tickets.new(ticket_params)
+  end
+
+  def create
+    @ticket = @event.tickets.new(ticker_params)
+    if @ticket.save
+      redirect_to admin_event_tickets_path(@event)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @ticket = @event.tickets.find(params[:id])
+  end
+
+  def update
+    @ticket = @event.tickets.find([:id])
+    if @ticket.update(ticket_params)
+      redirect_to admin_event_tickets_path(@event)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @ticket = @event.tickets.find([:id])
+    @ticket.destroy
+  end
+
+  protected
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:name, :price, :description)
+  end
+
+
 end
